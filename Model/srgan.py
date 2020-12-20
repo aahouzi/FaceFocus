5ff6eb19043f24dcee10e419b5e796720ef5ba56
Anas Ahouzi <ahouzi2000@hotmail.fr>
2020-12-20 15:02:10 +0100

    First commit

---
+++ Model/srgan.py
@@ -0,0 +1,79 @@
+
+from tensorflow.keras.layers import Add, BatchNormalization, Conv2D, Dense, Flatten, Input, LeakyReLU, PReLU, Lambda
+from tensorflow.keras.models import Model
+from Utils.utils import pixel_shuffle, normalize, denormalize_tanh, normalize_tanh
+
+
+def upsample(x_in, num_filters):
+    x = Conv2D(num_filters, kernel_size=3, padding='same')(x_in)
+    x = Lambda(pixel_shuffle(scale=2))(x)
+    return PReLU(shared_axes=[1, 2])(x)
+
+
+def res_block(x_in, num_filters, momentum=0.8):
+    x = Conv2D(num_filters, kernel_size=3, padding='same')(x_in)
+    x = BatchNormalization(momentum=momentum)(x)
+    x = PReLU(shared_axes=[1, 2])(x)
+    x = Conv2D(num_filters, kernel_size=3, padding='same')(x)
+    x = BatchNormalization(momentum=momentum)(x)
+    x = Add()([x_in, x])
+    return x
+
+
+def generator(lr_size, num_filters=64, num_res_blocks=16):
+    x_in = Input(shape=(lr_size, lr_size, 3))
+    x = Lambda(normalize)(x_in)
+
+    x = Conv2D(num_filters, kernel_size=9, padding='same')(x)
+    x = x_1 = PReLU(shared_axes=[1, 2])(x)
+
+    for _ in range(num_res_blocks):
+        x = res_block(x, num_filters)
+
+    x = Conv2D(num_filters, kernel_size=3, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Add()([x_1, x])
+
+    x = upsample(x, num_filters * 4)
+    x = upsample(x, num_filters * 4)
+
+    x = Conv2D(3, kernel_size=9, padding='same', activation='tanh')(x)
+
+    # Here, we denormalize to get an image with pixels in [0, 255]
+    x = Lambda(denormalize_tanh)(x)
+
+    return Model(x_in, x)
+
+
+def discriminator_block(x_in, num_filters, strides=1, batchnorm=True, momentum=0.8):
+    x = Conv2D(num_filters, kernel_size=3, strides=strides, padding='same')(x_in)
+    if batchnorm:
+        x = BatchNormalization(momentum=momentum)(x)
+    return LeakyReLU(alpha=0.2)(x)
+
+
+def discriminator(hr_size, num_filters=64):
+    x_in = Input(shape=(hr_size, hr_size, 3))
+    x = Lambda(normalize_tanh)(x_in)
+
+    x = discriminator_block(x, num_filters, batchnorm=False)
+    x = discriminator_block(x, num_filters, strides=2)
+
+    x = discriminator_block(x, num_filters * 2)
+    x = discriminator_block(x, num_filters * 2, strides=2)
+
+    x = discriminator_block(x, num_filters * 4)
+    x = discriminator_block(x, num_filters * 4, strides=2)
+
+    x = discriminator_block(x, num_filters * 8)
+    x = discriminator_block(x, num_filters * 8, strides=2)
+
+    x = Flatten()(x)
+
+    x = Dense(1024)(x)
+    x = LeakyReLU(alpha=0.2)(x)
+    x = Dense(1, activation='sigmoid')(x)
+
+    return Model(x_in, x)
+
+
