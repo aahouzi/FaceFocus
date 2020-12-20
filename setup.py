eaf983d9063ac4e1871a553121c339140f28befc
Anas Ahouzi <ahouzi2000@hotmail.fr>
2020-12-20 22:10:29 +0100

    New setup 0.0.2.2

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="FaceFocus-aahouzi",
-    version="0.0.2.1",
+    version="0.0.2.2",
     author="Anas Ahouzi",
     author_email="ahouzi2000@hotmail.fr",
     description="Generates high resolution face images from low resolution ones",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    # url="https://github.com/aahouzi/Simple_Document_Classification_From_Scratch.git",
+    url="https://github.com/aahouzi/FaceFocus-Project.git",
     packages=setuptools.find_packages(),
     classifiers=["Programming Language :: Python :: 3",
                  "License :: OSI Approved :: MIT License",
